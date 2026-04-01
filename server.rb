2476b0e76e91d76bd272425c1e67575bcef7622f
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2026-04-01 13:01:21 +0300

    Refactor server.rb for cleaner code structure

--- server.rb
+++ server.rb
@@ -1,21 +1,17 @@
+require 'bundler/setup'
 require 'webrick'
 require 'erb'
 
-# Налаштування порту та хоста для Render
+# Далі весь твій код, який ми писали минулого разу:
 port = ENV['PORT'] || 3000
-server = WEBrick::HTTPServer.new(
-  Port: port,
-  BindAddress: '0.0.0.0' # Це обов'язково для Render!
-)
+server = WEBrick::HTTPServer.new(Port: port, BindAddress: '0.0.0.0')
 
 server.mount_proc '/' do |req, res|
-  # Визначаємо шлях до файлу
   page_name = req.path == "/" ? "test" : req.path.delete_prefix("/")
   file_path = "#{page_name}.html.erb"
 
   if File.exist?(file_path)
     template = File.read(file_path)
-    # Обробка ERB
     renderer = ERB.new(template)
     res.body = renderer.result(binding)
     res.content_type = 'text/html; charset=utf-8'
