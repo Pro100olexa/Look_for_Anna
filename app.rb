6d5db7bdfa6ac1f8a2f568a014faf9db1e283d77
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2026-04-02 14:24:14 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 
+# Це важливо для Render, щоб він розумів, куди надсилати трафік
+set :bind, '0.0.0.0'
+port = ENV['PORT'] || 4567
+set :port, port
+
 get '/temperature' do
-  # Sinatra сама знайде файл temperature.html.erb у папці views
+  # Змінна params працює ТІЛЬКИ всередині цього блоку get
   erb :temperature
+end
+
+get '/' do
+  "Сервер працює! Перейдіть на /temperature"
 end
