31ddd0079ce8f4f301c6ff362bd972ced3f2d93a
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2026-04-10 22:17:05 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,16 +1,15 @@
 require 'sinatra'
+require 'ostruct' # Додай цей рядок обов'язково
 
-# Налаштування для Render
+set :server, 'webrick' # Це допоможе стабілізувати запуск
 set :bind, '0.0.0.0'
-port = ENV['PORT'] || 4567
-set :port, port
+set :port, ENV['PORT'] || 4567
 
 get '/' do
   "Сервер працює! Перейдіть на <a href='/temperature'>сторінку температури</a>"
 end
 
 get '/temperature' do
-  # Змінна params працює ТІЛЬКИ тут, коли користувач відкриває сторінку
   @temp = params['temp'] 
   erb :temperature
 end
