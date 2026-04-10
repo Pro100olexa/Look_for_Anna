7dc8a1765c4b0f387320a2163bb90e71616cb823
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2026-04-10 22:39:01 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,12 +5,19 @@ require 'ostruct'
 set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || 4567
 
+# Головна сторінка
 get '/' do
   "Сервер працює! Перейдіть на <a href='/temperature?temp=25'>сторінку температури</a>"
 end
 
+# Сторінка температури
 get '/temperature' do
-  # Використовуємо .to_s, щоб уникнути помилок, якщо параметр порожній
-  @temp = params['temp'].to_s 
-  erb :temperature
+  # Безпечне отримання параметра
+  @temp = params && params['temp'] ? params['temp'].to_s : "не вказана"
+  
+  begin
+    erb :temperature
+  rescue StandardError => e
+    "Помилка у файлі шаблону: #{e.message}"
+  end
 end
