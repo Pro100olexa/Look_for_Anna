d29704d132ea4b6e35348f8409d24cd10ef4fea0
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2026-04-10 22:48:03 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,23 +1,15 @@
 require 'sinatra'
 require 'ostruct'
 
-# Налаштування для Render
 set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || 4567
 
-# Головна сторінка
 get '/' do
-  "Сервер працює! Перейдіть на <a href='/temperature?temp=25'>сторінку температури</a>"
+  erb :temperature
 end
 
-# Сторінка температури
 get '/temperature' do
-  # Безпечне отримання параметра
-  @temp = params && params['temp'] ? params['temp'].to_s : "не вказана"
-  
-  begin
-    erb :temperature
-  rescue StandardError => e
-    "Помилка у файлі шаблону: #{e.message}"
-  end
+  # Беремо значення з URL і кладемо в ПЕРЕМІННУ @temp
+  @temp = params['temp'].to_s
+  erb :temperature
 end
