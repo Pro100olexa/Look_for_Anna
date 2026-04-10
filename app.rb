eb890b53cfd71430803f41ec3097fe0472e3cc37
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2026-04-10 22:49:50 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -5,11 +5,12 @@ set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || 4567
 
 get '/' do
+  # Відразу показуємо сторінку температури як головну
+  @temperature = params['temperature']
   erb :temperature
 end
 
 get '/temperature' do
-  # Беремо значення з URL і кладемо в ПЕРЕМІННУ @temp
-  @temp = params['temp'].to_s
+  @temperature = params['temperature']
   erb :temperature
 end
