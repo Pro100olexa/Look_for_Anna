7096fe386ed9a45588bf474fe843a4a607ca581c
Pro100olexa <93083786+Pro100olexa@users.noreply.github.com>
2026-04-11 11:20:28 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,17 +1,14 @@
 require 'sinatra'
-require 'ostruct'
 
 set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || 4567
 
 get '/' do
-  # Головна сторінка відразу показує форму
   @temp = ""
   erb :temperature
 end
 
 get '/temperature' do
-  # ВАЖЛИВО: використовуємо 'temp', бо так написано в name="temp" у твоєму HTML
-  @temp = params['temp'] || ""
+  @temp = params['temp'].to_s
   erb :temperature
 end
